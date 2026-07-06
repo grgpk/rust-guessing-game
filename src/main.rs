f3ac2cdce357273c7153eb4b9a94f96b96ebdcb4
Giorgi <grgpaikidze@gmail.com>
2026-07-07 02:51:40 +0400

    implement the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret = rand::random_range(1..=100);
+    
+    loop {
+        println!("Please input your guess.");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line");
+    
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("That's not a number, try again");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win! 🎉");
+                break;
+            } 
+        }   
+    }
 }
